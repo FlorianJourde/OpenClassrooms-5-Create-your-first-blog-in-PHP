f8d502ec4fab3d2eb33ccf610726edb5c1cdaf33
Florian Jourde <jourdeflorian@gmail.com>
2022-06-17 17:37:36 +0200

    Adding files after gitignore

--- ressources/blog/src/controllers/homepage.php
+++ ressources/blog/src/controllers/homepage.php
@@ -1,13 +1,12 @@
 <?php
-// controllers/homepage.php
 
-//require_once('src/model.php');
+require_once('src/lib/database.php');
 require_once('src/model/post.php');
 
 function homepage()
 {
-//  $posts = getPosts();
   $postRepository = new PostRepository();
+  $postRepository->connection = new DatabaseConnection();
   $posts = $postRepository->getPosts();
 
   require('templates/homepage.php');
--- ressources/blog/src/controllers/post.php
+++ ressources/blog/src/controllers/post.php
@@ -1,18 +1,15 @@
 <?php
-// controllers/post.php
 
-//require_once('src/model.php');
+require_once('src/lib/database.php');
 require_once('src/model/comment.php');
-//require_once('src/model/post.php');
+require_once('src/model/post.php');
 
 function post(string $identifier)
 {
   $postRepository = new PostRepository();
+  $postRepository->connection = new DatabaseConnection();
   $post = $postRepository->getPost($identifier);
-//  $post = $postRepository->getPost($identifier);
   $comments = getComments($identifier);
-//  $post = getPost($identifier);
-//  $comments = getComments($identifier);
 
   require('templates/post.php');
 }

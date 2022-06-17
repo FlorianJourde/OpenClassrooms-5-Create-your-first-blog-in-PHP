0bd035f557a8825ccae2f8e964707eaf2fd580b3
Florian Jourde <jourdeflorian@gmail.com>
2022-06-17 15:12:52 +0200

    Updating work with MVC course

--- ressources/blog/src/controllers/homepage.php
+++ ressources/blog/src/controllers/homepage.php
@@ -1,11 +1,14 @@
 <?php
 // controllers/homepage.php
 
-require_once('src/model.php');
+//require_once('src/model.php');
+require_once('src/model/post.php');
 
 function homepage()
 {
-  $posts = getPosts();
+//  $posts = getPosts();
+  $postRepository = new PostRepository();
+  $posts = $postRepository->getPosts();
 
   require('templates/homepage.php');
 }
--- ressources/blog/src/controllers/post.php
+++ ressources/blog/src/controllers/post.php
@@ -1,14 +1,18 @@
 <?php
 // controllers/post.php
 
-require_once('src/model.php');
+//require_once('src/model.php');
 require_once('src/model/comment.php');
-require_once('src/model/post.php');
+//require_once('src/model/post.php');
 
 function post(string $identifier)
 {
-  $post = getPost($identifier);
+  $postRepository = new PostRepository();
+  $post = $postRepository->getPost($identifier);
+//  $post = $postRepository->getPost($identifier);
   $comments = getComments($identifier);
+//  $post = getPost($identifier);
+//  $comments = getComments($identifier);
 
   require('templates/post.php');
 }

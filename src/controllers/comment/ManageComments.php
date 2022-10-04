3ad7f93c93cb27c657d4b592d8cff299e640acc8
unknown <florian@agence-cwa.fr>
2022-10-04 10:37:45 +0200

    Fix bug about comment name

--- src/controllers/comment/ManageComments.php
+++ src/controllers/comment/ManageComments.php
@@ -28,64 +28,36 @@ class ManageComments
 
         $userRole = new CheckUserRole();
 
-         if ($userRole->isAuthenticated($_SESSION['token'] ?? '')) {
-             if ($userRole->isAdmin($_SESSION['role']) ?? 'Guest') {
+        if ($userRole->isAuthenticated($_SESSION['token'] ?? '')) {
+            if ($userRole->isAdmin($_SESSION['role']) ?? 'Guest') {
 
-                 $hiddenComments = $commentRepository->getHiddenComments();
+                $hiddenComments = $commentRepository->getHiddenComments();
 
-                 $posts = [];
+                $posts = [];
 
-                 foreach ($hiddenComments as $comment) {
-//                     var_dump("Comment before : ", $comment);
-//                     var_dump($comment->user_id);
-//                     var_dump($userRepository->getUserFromId($comment->user_id)->username);
-//                     $comment->username = $userRepository->getUserFromId($comment->user_id)->username;
-//                     var_dump($userRepository->getUserFromId($comment->user_id)->username);
-                     $comment->username = $userRepository->getUserFromId($comment->user_id)->username;
-                     $comment->post = $postRepository->getPost($comment->post_id);
+                foreach ($hiddenComments as $comment) {
+                    $comment->username = $userRepository->getUserFromId($comment->user_id)->username;
+                    $comment->post = $postRepository->getPost($comment->post_id);
 
-//                     var_dump("Comment after : ", $comment);
-//                     var_dump($comment->post);
-//                     var_dump($comment->username);
-//                     var_dump($comment->post);
-//                     var_dump($userRepository->getUserFromId($comment->user_id)->username);
-//                     var_dump($post);
-//                     $comment->user = $userRepository->getUserFromId($comment->post->user_id)->username;
-//                     var_dump($comment);
-//                     var_dump($userRepository->getUserFromId($comment->post->user_id)->username);
-//                     die();
+                    if (!in_array($comment->post, $posts)) {
+                        $posts[] = $comment->post;
+                    }
+                }
 
-//                     var_dump($comment);
-
-                     if (!in_array($comment->post, $posts)) {
-//                         var_dump($comment->username);
-//                         var_dump($posts->username);
-//                     $posts[] = $comment->username;
-                         $posts[] = $comment->post;
-//                         var_dump($comment);
-                     }
-//                 var_dump($comment);
-                 }
-
-//                 var_dump($posts);
-
-                 foreach ($posts as $post) {
-//                     var_dump($post);
-//                     var_dump($commentRepository->getHiddenCommentsFromId($post->identifier));
-                     $post->hiddenComments = $commentRepository->getHiddenCommentsFromId($post->identifier);
-                     $post->username = $userRepository->getUserFromId($post->user_id)->username;
-//                     var_dump($post);
-//                     var_dump($post->user_id);
-//                     var_dump($userRepository->getUserFromId($post->user_id)->username);
-//                     var_dump($post->username = $userRepository->getUserFromId());
-                 }
-//             }
-             }
-         } else {
-             throw new \Exception('Vous n\'avez pas accès à cette page !');
-         }
+                foreach ($posts as $post) {
+                    $post->hiddenComments = $commentRepository->getHiddenCommentsFromId($post->identifier);
+                    $post->username = $userRepository->getUserFromId($post->user_id)->username;
+                    
+                    foreach ($post->hiddenComments as $hiddenComment) {
+                        $hiddenComment->username = $userRepository->getUserFromId($hiddenComment->user_id)->username;
+                    }
+                }
+            }
+        } else {
+            throw new \Exception('Vous n\'avez pas accès à cette page !');
+        }
 
         $twig = new RenderFront();
-        echo $twig->render('manage_comments.twig', [/* 'comments' => $comments,  */'posts' => $posts, 'session' => $_SESSION]);
+        echo $twig->render('manage_comments.twig', ['posts' => $posts, 'session' => $_SESSION]);
     }
 }

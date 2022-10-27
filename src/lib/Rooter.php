cff4ebb3cb334f0c4fb16c107c41c3740268d6a7
Florian Jourde <jourdeflorian@gmail.com>
2022-10-27 03:02:44 +0200

    Deploy modifications on server, adjust .htaccess and rooter paths

--- src/lib/Rooter.php
+++ src/lib/Rooter.php
@@ -2,26 +2,26 @@
 
 namespace Application\Lib;
 
-require_once 'Request.php';
-require_once 'Controller.php';
-require_once '../src/lib/Request.php';
-require_once '../src/controllers/AddComment.php';
-require_once '../src/controllers/DeleteComment.php';
-require_once '../src/controllers/HideComment.php';
-require_once '../src/controllers/ShowComment.php';
-require_once '../src/controllers/ManageComments.php';
-require_once '../src/controllers/UpdateComment.php';
-require_once '../src/controllers/AddPost.php';
-require_once '../src/controllers/DeletePost.php';
-require_once '../src/controllers/UpdatePost.php';
-require_once '../src/controllers/AuthenticationUser.php';
-require_once '../src/controllers/LogoutUser.php';
-require_once '../src/controllers/Archive.php';
-require_once '../src/controllers/Contact.php';
-require_once '../src/controllers/Homepage.php';
-require_once '../src/controllers/Post.php';
-require_once '../src/controllers/Register.php';
-require_once '../src/controllers/Login.php';
+require_once "Request.php";
+require_once "Controller.php";
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/lib/Request.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/AddComment.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/DeleteComment.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/HideComment.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/ShowComment.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/ManageComments.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/UpdateComment.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/AddPost.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/DeletePost.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/UpdatePost.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/AuthenticationUser.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/LogoutUser.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/Archive.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/Contact.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/Homepage.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/Post.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/Register.php");
+require_once ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/Login.php");
 
 use Exception;
 
@@ -47,7 +47,7 @@ class Rooter {
 
         // Création du nom du fichier du contrôleur
         $classController = $controller;
-        $fileController = "../src/controllers/" . $classController . ".php";
+        $fileController = ($_SERVER['DOCUMENT_ROOT'] . "/src/controllers/" . $classController . ".php");
         if (file_exists($fileController)) {
 //            die();
             $className = "Application\\Controllers\\".$controller;

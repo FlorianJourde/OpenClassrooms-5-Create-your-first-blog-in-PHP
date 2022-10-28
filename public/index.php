57bdb9102f0cd6a79f2433e976e66d400c025cc2
Florian Jourde <jourdeflorian@gmail.com>
2022-10-28 12:53:06 +0200

    Update index.php root

--- public/index.php
+++ public/index.php
@@ -1,8 +1,9 @@
 <?php
 
 define("Application\\Lib\\Root", $_SERVER['DOCUMENT_ROOT']);
+use const Application\Lib\Root;
 
-require_once '../src/lib/Rooter.php';
+require_once (Root . "/src/lib/Rooter.php");
 
 use Application\Lib\Rooter;
 

922992b850d15446c654c1f1ff38d382b7f99bb3
Florian Jourde <jourdeflorian@gmail.com>
2023-01-13 15:41:02 +0100

    Save changes before update with current online version

---
+++ public/ressources/css/main.js
@@ -0,0 +1,7 @@
+import momentumScroll from "/ressources/js/_momentum-scroll.js";
+import toggleMenu from "/ressources/js/_menu.js";
+import scrollTo from "/ressources/js/_scroll-to.js";
+
+momentumScroll();
+toggleMenu();
+scrollTo();

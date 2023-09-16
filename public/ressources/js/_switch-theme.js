c8b5e9d0e08f92eda6cc7b555a8a3c50ee79f159
Florian Jourde <jourdeflorian@gmail.com>
2023-09-16 04:12:54 +0200

    Finish dark mode

--- public/ressources/js/_switch-theme.js
+++ public/ressources/js/_switch-theme.js
@@ -1,20 +1,31 @@
 export default function switchTheme() {
-  const themeToggleBtn = document.getElementById("theme-toggle-button");
+  // const themeToggleButtons = document.getElementById('theme-toggle-button');
+  const themeToggleButtons = document.querySelectorAll('.theme-toggle-button');
   const body = document.body;
 
-  const savedTheme = localStorage.getItem("theme");
+  const savedTheme = localStorage.getItem('theme');
 
-  if (savedTheme === "dark") {
-    body.classList.add("dark");
+  if (savedTheme === 'dark') {
+    body.classList.add('dark');
+    // themeToggleButtons.classList.add('dark')
+    themeToggleButtons.forEach((element) => element.classList.add('dark'))
   }
 
-  themeToggleBtn.addEventListener("click", function () {
-    if (body.classList.contains("dark")) {
-      body.classList.remove("dark");
-      localStorage.setItem("theme", "");
-    } else {
-      body.classList.add("dark");
-      localStorage.setItem("theme", "dark");
-    }
-  });
+  themeToggleButtons.forEach(function(element) {
+      element.addEventListener('click', function () {
+      if (body.classList.contains('dark')) {
+        body.classList.remove('dark');
+        // themeToggleButtons.classList.remove('dark')
+        // themeToggleButtons.forEach((element) => element.classList.remove('dark'));
+        this.classList.remove('dark');
+        localStorage.setItem('theme', '');
+      } else {
+        body.classList.add('dark');
+        // themeToggleButtons.classList.add('dark');
+        // themeToggleButtons.forEach((element) => element.classList.add('dark'));
+        this.classList.add('dark');
+        localStorage.setItem('theme', 'dark');
+      }
+    });
+  })
 }

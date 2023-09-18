97b3a81a22cb3fe1e4e946f1d2f8b7244e5e6c01
Florian Jourde <jourdeflorian@gmail.com>
2023-09-18 23:53:38 +0200

    Update switch style

--- public/ressources/js/_switch-theme.js
+++ public/ressources/js/_switch-theme.js
@@ -1,13 +1,10 @@
 export default function switchTheme() {
-  // const themeToggleButtons = document.getElementById('theme-toggle-button');
   const themeToggleButtons = document.querySelectorAll('.theme-toggle-button');
   const body = document.body;
-
   const savedTheme = localStorage.getItem('theme');
 
   if (savedTheme === 'dark') {
     body.classList.add('dark');
-    // themeToggleButtons.classList.add('dark')
     themeToggleButtons.forEach((element) => element.classList.add('dark'))
   }
 
@@ -15,15 +12,11 @@ export default function switchTheme() {
       element.addEventListener('click', function () {
       if (body.classList.contains('dark')) {
         body.classList.remove('dark');
-        // themeToggleButtons.classList.remove('dark')
-        // themeToggleButtons.forEach((element) => element.classList.remove('dark'));
-        this.classList.remove('dark');
+        themeToggleButtons.forEach((element) => element.classList.remove('dark'))
         localStorage.setItem('theme', '');
       } else {
         body.classList.add('dark');
-        // themeToggleButtons.classList.add('dark');
-        // themeToggleButtons.forEach((element) => element.classList.add('dark'));
-        this.classList.add('dark');
+        themeToggleButtons.forEach((element) => element.classList.add('dark'))
         localStorage.setItem('theme', 'dark');
       }
     });

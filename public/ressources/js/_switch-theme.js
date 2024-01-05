131177bc59c45958c8f2974036c94563f85c341b
Florian Jourde <jourdeflorian@gmail.com>
2024-01-05 23:48:11 +0100

    Update logos & mobile header

--- public/ressources/js/_switch-theme.js
+++ public/ressources/js/_switch-theme.js
@@ -1,21 +1,22 @@
 export default function switchTheme() {
   const themeToggleButtons = document.querySelectorAll('.theme-toggle-button');
   const body = document.body;
+  const html = document.querySelector('html');
   const savedTheme = localStorage.getItem('theme');
 
   if (savedTheme === 'dark') {
-    body.classList.add('dark');
+    html.classList.add('dark');
     themeToggleButtons.forEach((element) => element.classList.add('dark'))
   }
 
   themeToggleButtons.forEach(function(element) {
       element.addEventListener('click', function () {
-      if (body.classList.contains('dark')) {
-        body.classList.remove('dark');
+      if (html.classList.contains('dark')) {
+        html.classList.remove('dark');
         themeToggleButtons.forEach((element) => element.classList.remove('dark'))
         localStorage.setItem('theme', '');
       } else {
-        body.classList.add('dark');
+        html.classList.add('dark');
         themeToggleButtons.forEach((element) => element.classList.add('dark'))
         localStorage.setItem('theme', 'dark');
       }
